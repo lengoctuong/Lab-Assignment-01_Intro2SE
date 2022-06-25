fc6d83dd102554795b08bfa72998762e61ad54a9
Tuong <lengoctuong23052002@gmail.com>
2022-06-25 23:17:30 +0700

    [06]_Patch

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,14 @@
 import express from 'express'
 import { v4 as uuidv4 } from 'uuid'
 
-const router = express.Router();
+const router = express.Router()
 var users = []
 
 router.get('/', (req, res) => res.send(users))
 
 router.post('/', (req, res) => {
     const user = req.body
-    users.push({ ...user, id: uuidv4() });
+    users.push({ ...user, id: uuidv4() })
     res.send(`User with name ${user.firstname} added to the database!`)
 })
 
@@ -22,6 +22,21 @@ router.delete('/:id', (req, res) => {
     const { id } = req.params
     users = users.filter((user) => user.id !== id)
     res.send(`User with the id ${id} deleted from the database!`)
+})
+
+router.patch('/:id', (req, res) => {
+    const { id } = req.params
+    const { firstname, lastname, age } = req.body
+    var user = users.find((user) => user.id === id)
+
+    if (firstname)
+        user.firstname = firstname
+    if (lastname)
+        user.lastname = lastname
+    if (age)
+        user.age = age
+    
+    res.send(`User with the id ${id} has been updated!`)
 })
 
 export default router
