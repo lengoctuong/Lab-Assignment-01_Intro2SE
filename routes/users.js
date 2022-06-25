3e16e218cc8de35c8cdc3f81c89774fe240ea77f
Tuong <lengoctuong23052002@gmail.com>
2022-06-25 22:56:47 +0700

    [05]_Delete

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ import express from 'express'
 import { v4 as uuidv4 } from 'uuid'
 
 const router = express.Router();
-const users = []
+var users = []
 
 router.get('/', (req, res) => res.send(users))
 
@@ -16,6 +16,12 @@ router.get('/:id', (req, res) => {
     const { id } = req.params
     const foundUser = users.find((user) => user.id === id)
     res.send(foundUser)
+})
+
+router.delete('/:id', (req, res) => {
+    const { id } = req.params
+    users = users.filter((user) => user.id !== id)
+    res.send(`User with the id ${id} deleted from the database!`)
 })
 
 export default router
