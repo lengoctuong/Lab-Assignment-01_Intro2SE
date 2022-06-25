5ee4e2917b540cf0dee79748943552ac9fa15749
Tuong <lengoctuong23052002@gmail.com>
2022-06-25 20:56:00 +0700

    [02]_GetBasic-Library

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-const express = require('express')
+import express from "express"
+import bodyParser from "body-parser"
+import morgan from "morgan"
+
+// const express = require('express')
+// const bodyParser = require('body-parser')
+// const morgan = require('morgan')
+
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port ${port}`))
+app.use(bodyParser.json())
+app.use(morgan('combined'))
+app.get('/', (req, res) => res.send('Hello Web'))
+app.listen(port, () => console.log(`Server running on port: http://localhost:${port}`))
